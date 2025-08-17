0f05a467682e1b321849d42ad1d3e7f145ec19b3
zaid444-max <zaidsherzad444@gmail.com>
2025-08-17 15:37:00 +0300

    Delete public directory

--- public/stock entry/stockFunctions.js
+++
@@ -1,9 +0,0 @@
-export function focusNextInput(input, inputType) {
-  input.addEventListener('keydown', function(e) {
-    if (e.key === 'Enter') {
-      const nextInp = e.target.closest('.container-div').nextElementSibling?.querySelector(`.${inputType}-inp`);
-      nextInp?.focus();
-      nextInp?.select();
-    }
-  })
-}
